d1d5e1d0b5feee9cb0f4224866cf7850edefb1e6
Pranshu Saurav <pranshusaurav04@gmail.com>
2018-01-07 19:53:48 +0530

    changed in MainActivity, AsyncTask to LoaderManager

--- app/src/main/java/com/example/prans/booklist/MainActivity.java
+++ app/src/main/java/com/example/prans/booklist/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.prans.booklist;
 
+import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -12,11 +16,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Book>> {
 
     private BookAdapter mAdapter;
 
@@ -27,14 +32,17 @@ public class MainActivity extends AppCompatActivity {
     private ListView bookListView;
     TextView mEmptyStateTextView;
 
+    private static final int BOOK_LOADER_ID = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
-        final EditText search_query = (EditText) findViewById(R.id.etSearch);
-        Button mSearch = (Button) findViewById(R.id.btnSearch);
+        Log.d(LOG_TAG, "onCreate.....");
+        mEmptyStateTextView = findViewById(R.id.empty_view);
+        final EditText search_query = findViewById(R.id.etSearch);
+        Button mSearch = findViewById(R.id.btnSearch);
         mSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,13 +56,26 @@ public class MainActivity extends AppCompatActivity {
 
                     etQuery = etQuery.replaceAll(" ", "+");
                     GOOGLE_REQUEST_URL = "https://www.googleapis.com/books/v1/volumes?q=" + etQuery;
-                    BookAsyncTask task = new BookAsyncTask();
-                    task.execute(GOOGLE_REQUEST_URL);
+
+                    ConnectivityManager connMgr = (ConnectivityManager)
+                            getSystemService(Context.CONNECTIVITY_SERVICE);
+
+                    NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+                    if (networkInfo != null && networkInfo.isConnected()) {
+
+                        LoaderManager loaderManager = getLoaderManager();
+                        loaderManager.initLoader(BOOK_LOADER_ID, null, this);  // error here on 3rd argument
+
+                    } else {
+                        Toast.makeText(MainActivity.this, "No internet available", Toast.LENGTH_SHORT).show();
+                    }
+
                 }
 
             }
         });
-        bookListView = (ListView) findViewById(R.id.list);
+        bookListView = findViewById(R.id.list);
         mAdapter = new BookAdapter(this, new ArrayList<Book>());
         bookListView.setAdapter(mAdapter);
 
@@ -70,31 +91,29 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public Loader<List<Book>> onCreateLoader(int i, Bundle bundle) {
 
-    private class BookAsyncTask extends AsyncTask<String, Void, List<Book>> {
-        @Override
-        protected List<Book> doInBackground(String... urls) {
-            Log.d(LOG_TAG, "doInBackground process started.....");
-            if (urls.length < 1 || urls[0] == null) {
-                return null;
-            }
-            List<Book> result = QueryUtils.fetchBooksData(urls[0]);
-            return result;
+        Log.d(LOG_TAG, "onCreateLoader.....");
+        return new BookLoader(this, GOOGLE_REQUEST_URL);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Book>> loader, List<Book> books) {
+
+        Log.d(LOG_TAG, " onLoadFinished.....");
+        mAdapter.clear();
+        if (books != null && !books.isEmpty()) {
+            mAdapter.setNotifyOnChange(true);
+            mAdapter.addAll(books);
         }
+    }
 
-        @Override
-        protected void onPostExecute(List<Book> data) {
-            Log.d(LOG_TAG, "onPostExecute process started.....");
+    @Override
+    public void onLoaderReset(Loader<List<Book>> loader) {
+        Log.d(LOG_TAG, " onLoaderReset.....");
 
-            View loadingIndicator = findViewById(R.id.loading_indicator);
-            loadingIndicator.setVisibility(View.GONE);
-
-            mEmptyStateTextView.setText(R.string.no_books);
-            mAdapter.clear();
-            if (data != null && !data.isEmpty()) {
-                mAdapter.addAll(data);
-                Log.d(LOG_TAG, "Books added.....");
-            }
-        }
+        mAdapter.clear();
+        mAdapter.notifyDataSetChanged();
     }
 }
